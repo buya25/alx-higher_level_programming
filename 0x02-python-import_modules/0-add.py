57db0c9e20c591127ce585208e8b9851717d5c32
Yabs Mullo <yabsmullo0@gmail.com>
2023-08-11 13:32:46 -0700

    n

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def my_custom_addition(a, b):
+    """My custom addition function
+
+    Args:
+        a: first operand
+        b: second operand
+
+    Returns:
+        The sum of a and b
+    """
+    return a + b
+
+result = my_custom_addition(1, 2)
+print("Result of addition: {}".format(result))
